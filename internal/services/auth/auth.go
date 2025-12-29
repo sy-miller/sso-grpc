a9ffa0342d037cc840d5ea3192f97b9cdc69c97d
stopcha <usrcrzy@gmail.com>
2025-12-29 19:13:13 +0200

    Define storage models, interfaces

--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -1,11 +1,40 @@
 package auth
 
-import "context"
+import (
+	"context"
+	"log/slog"
 
-type AuthSvc struct{}
+	"github.com/sy-miller/sso-grpc/internal/domain/models"
+)
 
-func New() *AuthSvc {
-	return &AuthSvc{}
+type AuthSvc struct {
+	log          *slog.Logger
+	userSaver    UserSaver
+	userProvider UserProvider
+	appProvider  AppProvider
+}
+
+type UserSaver interface {
+	SaveUser(ctx context.Context, email string, passHash []byte) (string, error)
+}
+
+type UserProvider interface {
+	GetUserByEmail(ctx context.Context, email string) (models.User, error)
+	IsAdmin(ctx context.Context, userId string) (bool, error)
+}
+
+type AppProvider interface {
+	GetAppById(ctx context.Context, appId int) (models.App, error)
+}
+
+// New returns a new instance of Auth service
+func New(log *slog.Logger, userSaver UserSaver, userProvider UserProvider, appProvider AppProvider) *AuthSvc {
+	return &AuthSvc{
+		log: log,
+		userSaver:    userSaver,
+		userProvider: userProvider,
+		appProvider:  appProvider,
+	}
 }
 
 func (a *AuthSvc) Login(ctx context.Context, email string, password string, appId int) (string, error) {
