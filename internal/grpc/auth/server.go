6039a404a982d62d257f24d15437d3887d6bcc87
stopcha <usrcrzy@gmail.com>
2025-12-31 02:28:13 +0200

    Fix code for ErrUserNotFound

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -76,7 +76,7 @@ func (s *serverAPI) IsAdmin(ctx context.Context, req *ssov1.IsAdminRequest) (*ss
 	isAdmin, err := s.auth.IsAdmin(ctx, req.GetUserId())
 	if err != nil {
 		if errors.Is(err, auth.ErrUserNotFound) {
-			return nil, status.Error(codes.AlreadyExists, "user already exists")
+			return nil, status.Error(codes.NotFound, "user not found")
 		}
 		return nil, status.Error(codes.Internal, "internal error")
 	}
